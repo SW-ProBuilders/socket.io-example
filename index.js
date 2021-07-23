b10f09ad7f20fa473d0b7c71b2b623fdee5a055d
wooffighter <keffurdg@gmail.com>
2021-07-23 19:03:37 +0200

    added ping arg text input
    -Added input/changed button into form
    -Removed response div cause fuck my plans ngl
    -Fixed comment in index.js

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ const server = createServer(app);
 
 const io = require("socket.io")(server);
 
-//Send basic ticket data
+//Connected event
 io.on('connection', (socket) => {
     console.log("Connected");
+
+    //Event recieved by server from client
     socket.on("ping", (args) => {
       console.log(`PONG (${args})`)
+
+      //reply with different event
       socket.emit("pong", args)
     })
 });
 
+
+//webserver bullshit
 app.all('/', function (req, res, next) {
     res.sendFile(path.join(__dirname)+"/index.html")
 })
